295ad47f39f93a3f030a168a81fc941109c105f3
jhlee <jhlee89898989@gmail.com>
2026-03-19 14:47:31 +0900

    add: 네비게이션 바 높이 상수로 선언

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,7 @@ export const API = {
   BASE_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api',
   TIMEOUT: 10000,
 } as const;
+
+export const STYLE = {
+  NAVBAR_HEIGHT: '4rem',
+};
