e1fb8dfb00b2dabf9cc4c9d011168625cfbf709b
jhlee <jhlee89898989@gmail.com>
2026-03-19 13:48:07 +0900

    add: 스크롤바 가리기 플러그인 설정하기
    플러그인 taillwindcss 설정 파일에 삽입

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,17 @@
-import { heroui } from "@heroui/react";
+import { heroui } from '@heroui/react';
+import tailWindScrollbarHide from 'tailwind-scrollbar-hide';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [heroui()],
+  darkMode: 'class',
+  plugins: [heroui(), tailWindScrollbarHide],
 };
 
 export default config;
