36266aa212f0e63f7664e7f9bdb593d8d4143608
youngsik <syssys6831@gmail.com>
2026-03-21 00:17:21 +0900

    fix: useNavigate 사용하기 위해 ClientRouter 파일을 통해 <Router>로 감쌈

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import NavigationBar from '@/components/navigationBar';
 import { headers } from 'next/headers';
 import { userAgent } from 'next/server';
+import TodayGoalDashboard from '@/components/today-goal/TodayGoalDashboard';
+import ClientRouter from '@/components/ClientRouter';
 
 const mobileClasses =
   'flex max-w-dvw min-w-dvw max-h-dvh min-h-dvh flex-col overflow-y-auto scrollbar-hide bg-amber-200';
@@ -14,168 +16,15 @@ export default async function Home() {
 
   return (
     <main className={isMobile ? mobileClasses : desktopClasses}>
-      {/* 
-        오버플로우 착각하지 않기 위해 넣어둔 요소입니다. 
-        삭제 후 작업하기
-      */}
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <div>hellow</div>
-      <NavigationBar />
+      <ClientRouter>
+        <div className="flex-1 p-4 pb-24 flex flex-col gap-6">
+          {/* 페이스 다이얼 */}
+          {/* 오늘 총 공부시간 */}
+          {/* 오늘의 목표 달성률 */}
+          <TodayGoalDashboard />
+        </div>
+        <NavigationBar />
+      </ClientRouter>
     </main>
   );
 }
