93bd372b61abe1375fc8331139d15bb58f1a7af5
jhlee <jhlee89898989@gmail.com>
2026-03-20 17:41:14 +0900

    feat: 목표 상세페이지 틀 작업

--- src/app/g/[goal_id]/page.tsx
+++ src/app/g/[goal_id]/page.tsx
@@ -1,6 +1,18 @@
+import DailyGoalList from './components/DailyGoalList';
+
 export default function GoalDetail() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-8">
+    <main className="flex w-dvw min-h-screen flex-col items-center justify-center gap-6 p-8">
+      {/*
+       * 목표 세부 정보 컴포넌트
+       */}
+      {/*
+       * 목표 진행도 컴포넌트
+       */}
+      {/*
+       * 목표 총 공부시간
+       */}
+      <DailyGoalList />
       목표 상세페이지
     </main>
   );
