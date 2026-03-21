d58c6d0df4dcac6d6957926ed2615cce9e68c6e8
youngsik <syssys6831@gmail.com>
2026-03-21 20:33:20 +0900

    refactor: 대시보드페이지 미사용 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 import NavigationBar from '@/components/navigationBar';
-import { headers } from 'next/headers';
-import { userAgent } from 'next/server';
 import TodayGoalDashboard from '@/components/today-goal/TodayGoalDashboard';
 import ClientRouter from '@/components/ClientRouter';
-
-const mobileClasses =
-  'flex max-w-dvw min-w-dvw max-h-dvh min-h-dvh flex-col overflow-y-auto scrollbar-hide bg-amber-200';
-const desktopClasses =
-  'relative flex max-w-[430px] min-w-[430px] max-h-[932px] min-h-[430px] rounded-2xl flex-col overflow-y-auto scrollbar-hide bg-amber-200';
 
 export default async function Home() {
   return (
