5797cb050a90a9f218246b7d5ebd60fd08bc7f39
youngsik <syssys6831@gmail.com>
2026-03-21 20:49:24 +0900

    feat: 대시보드 페이지, 데일리 목표 상세 페이지 위 페이지명 기재

--- src/app/goals/[goal_id]/[daily_id]/page.tsx
+++ src/app/goals/[goal_id]/[daily_id]/page.tsx
@@ -6,9 +6,9 @@ export default function DailyGoalDetailPage() {
   return (
     <ClientRouter>
       <div className="flex-1 min-h-screen w-full p-4 flex flex-col pb-24 overflow-y-auto scrollbar-hide bg-[#2c2c2c]">
+        <p className="text-white">데일리 목표 상세 페이지</p>
         {/* 목표 정보(목표명, 할당량, 시간, 중지버튼 */}
         {/* 오늘의 목표 달성률 */}
-
         <TodayGoalDailyDetail />
       </div>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default async function Home() {
   return (
     <ClientRouter>
       <div className="flex-1 p-4 pb-24 flex flex-col gap-6">
+        대시보드 페이지
         {/* 페이스 다이얼 */}
         {/* 오늘 총 공부시간 */}
         {/* 오늘의 목표 달성률 */}
