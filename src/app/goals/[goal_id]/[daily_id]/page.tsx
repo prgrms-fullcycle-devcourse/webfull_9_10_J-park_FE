0cbafd58d522a858a2eaca23b59ed940e967b087
youngsik <syssys6831@gmail.com>
2026-03-22 22:00:55 +0900

    오늘의 목표(데일리 목표 상세 페이지) 구현 (#14)
    * add : react-router 라이브러리 설치
    
    * feat: API 데이터 타입 설정 및 초반 파일 틀 작성
    
    * feat: 밀리초 00:00:00 형태로 변환 함수 추가
    
    * feat: 서버 데이터 호출을 위한 커스텀 훅 구현
    
    * feat: 달성&미달성 뱃지 구현
    
    * feat: 재생버튼, 정지버튼  UI 구현
    
    * feat: 대시보드용, 상세 목표페이지용 메인 컴포넌트 완성
    
    * fix: useNavigate 사용하기 위해 ClientRouter 파일을 통해 <Router>로 감쌈
    
    * design: heroui Card, Chip 적용
    test : 더미데이터 추가
    feat: Drag&Drop 구현
    
    * design: 대시보드의 오늘의 목표 디자인 변경 (둥근 박스 안에 배치, 목표 사이 간격 없앰)
    
    * fix: className isMobile 미사용
    
    * design: 오늘의 목표(데일리목표) 대시보드와 일치하게 디자인 수정
    
    * feat: 데일리 목표 상세 페이지 오늘의 목표 구현(카드형식, DnD)
    
    * refactor: 대시보드페이지 미사용 코드 삭제
    
    * feat: 할당량 표기 변경 (오늘 할당량 0/10페이지 -> 0 / 10페이지)
    
    * feat: 대시보드 페이지, 데일리 목표 상세 페이지 위 페이지명 기재
    
    * feat: 카드 높이 간격 통일

--- src/app/goals/[goal_id]/[daily_id]/page.tsx
+++ src/app/goals/[goal_id]/[daily_id]/page.tsx
@@ -1,7 +1,19 @@
+import NavigationBar from '@/components/navigationBar';
+import ClientRouter from '@/components/ClientRouter';
+import TodayGoalDailyDetail from '@/components/today-goal/TodayGoalDailyDetail';
+
 export default function DailyGoalDetailPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-8">
-      데일리 목표 상세페이지
-    </main>
+    <ClientRouter>
+      <div className="flex-1 min-h-screen w-full overflow-y-auto scrollbar-hide bg-[#2c2c2c]">
+        <div className="flex-1 p-4 pb-24 flex flex-col gap-6">
+          <p className="text-white">데일리 목표 상세 페이지</p>
+          {/* 목표 정보(목표명, 할당량, 시간, 중지버튼 */}
+          {/* 오늘의 목표 달성률 */}
+          <TodayGoalDailyDetail />
+        </div>
+      </div>
+      <NavigationBar />
+    </ClientRouter>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 import NavigationBar from '@/components/navigationBar';
-import { headers } from 'next/headers';
-import { userAgent } from 'next/server';
 import TodayGoalDashboard from '@/components/today-goal/TodayGoalDashboard';
 import ClientRouter from '@/components/ClientRouter';
-
-const mobileClasses =
-  'flex max-w-dvw min-w-dvw max-h-dvh min-h-dvh flex-col overflow-y-auto scrollbar-hide bg-amber-200';
-const desktopClasses =
-  'relative flex max-w-[430px] min-w-[430px] max-h-[932px] min-h-[430px] rounded-2xl flex-col overflow-y-auto scrollbar-hide bg-amber-200';
 
 export default async function Home() {
   return (
     <ClientRouter>
       <div className="flex-1 p-4 pb-24 flex flex-col gap-6">
+        대시보드 페이지
         {/* 페이스 다이얼 */}
         {/* 오늘 총 공부시간 */}
         {/* 오늘의 목표 달성률 */}
