910581eca2ce48e08de6c582a883b817a642f48d
youngsik <syssys6831@gmail.com>
2026-03-25 16:03:06 +0900

    feat: 페이스다이얼 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import NavigationBar from '@/components/navigationBar';
 import TodayGoalDashboard from '@/app/components/TodayGoalDashboard';
 import TodayGoalRatio from '@/app/components/TodayGoalRatio';
+import PaceDial from './components/PaceDial';
 
 export default async function Home() {
   return (
     <>
       <div className="flex-1 p-4 pb-24 flex flex-col gap-6">
-        {/* 페이스 다이얼 */}
-        {/* 오늘 총 공부시간 */}
+        <PaceDial />
         <TodayGoalRatio />
 
         <TodayGoalDashboard />
