6af6fe8091a6f624707adc39d2b69d7e2d4af943
youngsik <syssys6831@gmail.com>
2026-03-27 14:32:11 +0900

    feat: 대시보드 페이지 오늘의 목표 api 연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,48 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { Spinner } from '@heroui/react';
 import NavigationBar from '@/components/navigationBar';
 import TodayGoalDashboard from '@/app/components/TodayGoalDashboard';
 import TodayGoalRatio from '@/app/components/TodayGoalRatio';
 import PaceDial from './components/PaceDial';
 
-export default async function Home() {
+import { fetchTodayGoals } from '@/api/goalApi';
+
+export default function Home() {
+  const {
+    data: goalData,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['todayGoals'],
+    queryFn: fetchTodayGoals,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <Spinner color="warning" size="lg" />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="flex justify-center items-center min-h-screen text-red-500 font-bold">
+        데이터를 불러오는 중 오류가 발생했습니다.
+      </div>
+    );
+  }
+
+  const todayGoals = goalData?.data.todayGoals || [];
+
   return (
     <>
       <div className="flex-1 p-4 pb-24 flex flex-col gap-6">
         <PaceDial />
         <TodayGoalRatio />
-
-        <TodayGoalDashboard />
+        <TodayGoalDashboard goals={todayGoals} />
       </div>
 
       <NavigationBar />
