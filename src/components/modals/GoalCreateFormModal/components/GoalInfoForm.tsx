9b5a83de00f201c746dc68916c37f9c4ff586150
jhlee <jhlee89898989@gmail.com>
2026-03-31 21:30:09 +0900

    fix: 오타 수정

--- src/components/modals/GoalCreateFormModal/components/GoalInfoForm.tsx
+++ src/components/modals/GoalCreateFormModal/components/GoalInfoForm.tsx
@@ -27,7 +27,7 @@ export default function GoalInfoForm() {
         size="lg"
         variant="bordered"
         label="목표설명"
-        placeholder="묙표에 대해 간략하게 설명해주세요."
+        placeholder="목표에 대해 간략하게 설명해주세요."
       />
     </section>
   );
