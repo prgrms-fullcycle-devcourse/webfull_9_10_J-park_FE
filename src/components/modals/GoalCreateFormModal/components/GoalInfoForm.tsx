0dea29115b59ba810b996e90631770c6ed79741b
jhlee <jhlee89898989@gmail.com>
2026-03-31 13:55:53 +0900

    fix: 비제어 컴포넌트 경고 수정
    제어 컴포넌트는 값을 제공해야하는데 값을 undefined로 제공하여 비제어 컴포넌트로 인식해 던지는 경고 였습니다.

--- src/components/modals/GoalCreateFormModal/components/GoalInfoForm.tsx
+++ src/components/modals/GoalCreateFormModal/components/GoalInfoForm.tsx
@@ -22,7 +22,7 @@ export default function GoalInfoForm() {
         }}
       />
       <Textarea
-        value={detail}
+        value={detail || ''}
         onValueChange={setDetail}
         size="lg"
         variant="bordered"
