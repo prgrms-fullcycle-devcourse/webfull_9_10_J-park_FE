d41aba1779caeaccce2919ecf55cdb3a2f3e5fee
jhlee <jhlee89898989@gmail.com>
2026-03-31 21:20:57 +0900

    cleanup: 사용하지 않는 코드 삭제

--- src/components/FloatingTimer/index.tsx
+++ src/components/FloatingTimer/index.tsx
@@ -1,16 +1,12 @@
 'use client';
 import { api } from '@/lib/axios';
-import { Button, useDisclosure } from '@heroui/react';
 import { useEffect, useMemo, useState } from 'react';
-import { FaStop } from 'react-icons/fa6';
 import { useParams, usePathname } from 'next/navigation';
 
-import { EndTimerResponse, RunningTimerResponse } from '@/types/timer';
+import { RunningTimerResponse } from '@/types/timer';
 import Timer from './components/Timer';
 import { STORAGE_KEYS } from '@/constants';
 
-import { useMutation } from '@tanstack/react-query';
-import { endTimer } from '@/api/timerApi';
 import StopTimerModal from './components/StopTimerModal';
 
 export default function FloatingTimer() {
@@ -21,15 +17,6 @@ export default function FloatingTimer() {
   const [targetAmount, setTargetAmount] = useState(0);
   const [goalTitle, setGoalTitle] = useState('');
   const [isMinimized, setIsMinimized] = useState(false);
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const endMutation = useMutation({
-    mutationFn: ({ goalID, amount }: { goalID: string; amount: number }) =>
-      endTimer({
-        goalId: Number(goalID),
-        currentCompletedAmount: amount,
-        isPaused: false,
-      }),
-  });
 
   useEffect(() => {
     if (localStorage) {
