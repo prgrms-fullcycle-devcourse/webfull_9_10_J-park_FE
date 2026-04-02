cf0eabcc97320c464d94326aaeaedcec746ddf0f
jhlee <jhlee89898989@gmail.com>
2026-04-02 11:06:40 +0900

    fix: ping animation fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ const config = {
     './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    keyframes: {
+      ping: {
+        '75%, 100%': { transform: 'scale(1.2)', opacity: '0' },
+      },
+    },
+    // 2. Map the keyframes to utility names
+    animation: {
+      ping: 'ping 1s ease-in-out infinite',
+    },
     fontFamily: {
       notosan: ['NotoSan', 'sans-serif'],
     },
