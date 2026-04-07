3d055a3b503e59de5f67472243250e4c3a1f85f3
jhlee <jhlee89898989@gmail.com>
2026-04-07 14:11:05 +0900

    change: Home -> DashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,13 +2,12 @@
 
 import NavigationBar from '@/components/navigationBar';
 import UserInfoCard from './components/UserInfoCard';
-
 import PaceIndicatorCard from './components/PaceIndicatorCard';
 import TodayDailyGoalsCard from './components/TodayDailyGoalsCard';
 import TodayStudyInfoCard from './components/TodayStudyInfoCard';
 import OtherInfoCard from './components/OtherInfoCard';
 
-export default function Home() {
+export default function DashboardPage() {
   return (
     <>
       <div className="w-full flex-1 p-4 pb-24 flex flex-col gap-4">
@@ -16,7 +15,6 @@ export default function Home() {
         <PaceIndicatorCard />
         <TodayStudyInfoCard />
         <OtherInfoCard />
-
         <TodayDailyGoalsCard />
       </div>
       <NavigationBar />
