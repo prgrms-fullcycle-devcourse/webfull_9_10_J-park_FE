cf266c1dc51ed5a2d92422e80181f766cfd500b5
jhlee <jhlee89898989@gmail.com>
2026-04-15 13:17:08 +0900

    fix: 오늘 목표에서 자기 자신 목표는 제외

--- src/app/goals/[goal_id]/[daily_id]/components/DailyGoalList.tsx
+++ src/app/goals/[goal_id]/[daily_id]/components/DailyGoalList.tsx
@@ -5,51 +5,59 @@ import { TodayGoal } from '@/types/goal';
 import { IoChevronForwardOutline } from 'react-icons/io5';
 import { FcSurvey } from 'react-icons/fc';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
 
 interface DailyGoalListProps {
   goals: TodayGoal[];
 }
 
 export default function DailyGoalList({ goals }: DailyGoalListProps) {
+  const { goal_id } = useParams();
+  const goalID = Number(goal_id);
+
   return (
     <div className="animate-fadeIn flex flex-col">
       <small className="text-gray-600 p-6 pb-4">오늘 목표</small>
       <div className="flex flex-col">
-        {goals.map((goal, index) => (
-          <Link
-            href={`/goals/${goal.id}/${goal.goalLogId}`}
-            key={goal.id ?? `daily-goal-${index}`}
-            className="flex w-full h-full justify-between px-6 py-2"
-          >
-            <div className="flex gap-2">
+        {goals.map((goal, index) =>
+          goalID !== goal.id ? (
+            <Link
+              href={`/goals/${goal.id}/${goal.goalLogId}`}
+              key={goal.id ?? `daily-goal-${index}`}
+              className="flex w-full h-full justify-between px-6 py-2"
+            >
+              <div className="flex gap-2">
+                <Button
+                  radius="full"
+                  className="p-0 hover:cursor-default bg-primary"
+                  isIconOnly
+                  disableAnimation
+                  disableRipple
+                >
+                  <FcSurvey size={24} />
+                </Button>
+                <span className="text-left">
+                  <p className="text-xl font-black -mb-1">{goal.title}</p>
+                  <small className="text-gray-600">
+                    {goal.currentAmount}/{goal.targetAmount} {goal.unit}
+                  </small>
+                </span>
+              </div>
               <Button
                 radius="full"
-                className="p-0 hover:cursor-default bg-primary"
+                className="p-0"
                 isIconOnly
                 disableAnimation
                 disableRipple
+                variant="light"
               >
-                <FcSurvey size={24} />
+                <IoChevronForwardOutline className="text-gray-600" size={20} />
               </Button>
-              <span className="text-left">
-                <p className="text-xl font-black -mb-1">{goal.title}</p>
-                <small className="text-gray-600">
-                  {goal.currentAmount}/{goal.targetAmount} {goal.unit}
-                </small>
-              </span>
-            </div>
-            <Button
-              radius="full"
-              className="p-0"
-              isIconOnly
-              disableAnimation
-              disableRipple
-              variant="light"
-            >
-              <IoChevronForwardOutline className="text-gray-600" size={20} />
-            </Button>
-          </Link>
-        ))}
+            </Link>
+          ) : (
+            <></>
+          ),
+        )}
       </div>
     </div>
   );
