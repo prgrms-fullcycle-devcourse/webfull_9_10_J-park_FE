619d8523053c3d6161ecb7688d726b17788e9866
youngsik <syssys6831@gmail.com>
2026-04-15 17:39:50 +0900

    design: 카카오 로그인버튼 디자인 수정 (#84)

--- src/app/me/components/KakaoLoginCard/index.tsx
+++ src/app/me/components/KakaoLoginCard/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Button } from '@heroui/react';
-import { RiKakaoTalkFill } from 'react-icons/ri';
 
 export default function KakaoLoginCard() {
   const handleKakaoLogin = () => {
@@ -17,9 +16,25 @@ export default function KakaoLoginCard() {
       <Button
         size="sm"
         radius="full"
-        className="bg-primary text-white font-bold h-8 px-4 text-xs shadow-sm shadow-primary/20"
+        className="bg-[#FEE500] hover:bg-[#FEE500]/90 text-black/85 font-bold h-8 px-4 text-xs shadow-sm"
         onPress={handleKakaoLogin}
-        startContent={<RiKakaoTalkFill size={14} />}
+        disableRipple
+        startContent={
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 18 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M9 2C4.029 2 0 4.673 0 7.973C0 10.154 1.517 12.062 3.861 13.097L3.033 16.14C2.96 16.411 3.25 16.63 3.491 16.471L7.151 13.988C7.75 14.07 8.368 14.116 9 14.116C13.971 14.116 18 11.443 18 8.143C18 4.843 13.971 2 9 2Z"
+              fill="#000000"
+            />
+          </svg>
+        }
       >
         연동하기
       </Button>
